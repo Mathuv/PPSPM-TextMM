4ad4cb2e29c7f9c603e2aafedabf4eaea9a418f3
Mathu Mounasamy <mmounasamy@gmail.com>
2016-06-08 09:03:18 +1000

    Modified the file to run it as a script with db file as a parameter in command prompt, made the code more redable

--- textual_processor/programs/program.py
+++ textual_processor/programs/program.py
@@ -3,19 +3,21 @@ import nltk
 import re
 import os
 import errno
+import sys
 from types import *
 from nltk.corpus import stopwords
 
 
-# extract History of Present Illness - include step numbers here
+# extract History of Present Illness - step 2
 def extract_hpi(text):
     match = re.search(r'History of Present Illness:\s+((\S+\s)+)',text,re.IGNORECASE)
     return match.group(1) if match  else match
     
-# tokenize text into list of words
+# tokenize text into list of words - step 3
 def tokenize(text):
     return re.split(r'[ \t\n]+', text) if text else text
 
+# remove stop words - step 4
 def remove_stopwords(tokens):
     stopwords = nltk.corpus.stopwords.words('english')
     return [word for word in tokens if word not in stopwords] if tokens else tokens
@@ -48,14 +50,14 @@ def write_file(content, file):
     f.close()
 
 
-# dbfilename can be passed as command line argument - dbfilename = sys.argv[0]
-dbfilename = 'NOTEEVENTS_DATA_TABLE_PARTIAL_20REC'
-dbfile = open('..'+os.sep+'database'+os.sep+dbfilename+'.csv')
-dbreader = csv.reader(dbfile)
+dbfile = sys.argv[1]
+#dbfile = '../database/NOTEEVENTS_DATA_TABLE_PARTIAL_20REC.csv'
+dbpath = os.path.dirname(dbfile)
+dbfilename_ext = os.path.basename(dbfile)
+dbfilename = os.path.splitext(dbfilename_ext)[0]
+dbreader = csv.reader(open(dbfile))
 dbdata = list(dbreader)
 
-stopwords = nltk.corpus.stopwords.words('english')
-content =[]
 
 header_rec = dbdata[:1]  #Patient table column headers
 
@@ -69,15 +71,15 @@ for row in dbdata[1:]:
     #unprocessed data
     dsr_list.append(row[0::10])    #why 10? is the number of rows 10 in this dataset? - looks like hard-coded
     
-    # extract Histry of Present Illness
+    # extract Histry of Present Illness - step 2
     row[10] = extract_hpi(row[10])
     hpi_orginal.append(row[0::10])
 
-    # create tokenized list
+    # create tokenized list - step 3
     row[10] = tokenize(row[10])
     hpi_list_tokenized.append(row[0::10])
 
-    # create sstop word removed list
+    # create sstop word removed list - step 4
     row[10] = remove_stopwords(row[10])
     hpi_list_stpwd_rm.append(row[0::10])
 
@@ -85,22 +87,22 @@ for row in dbdata[1:]:
 
 
 
-# write dischard summery text csv
-raw_filename = '..'+os.sep+'database'+os.sep+'step1'+os.sep+dbfilename+'_RAW.csv'
+# write discharge summery text csv - step1
+raw_filename = dbpath+os.sep+'step1'+os.sep+dbfilename+'_RAW.csv'
 write_file(dsr_list,raw_filename)
 
 
-# write 'History of Present Illness' text csv
-hpi_filename = '..'+os.sep+'database'+os.sep+'step2'+os.sep+dbfilename+'_HPI.csv'
+# write 'History of Present Illness' text csv - step2
+hpi_filename = dbpath+os.sep+'step2'+os.sep+dbfilename+'_HPI.csv'
 write_file(hpi_orginal,hpi_filename)
 
 
-# write tokenized  'History of Present Illness' text file
-hpi_tokenized_filename = '..'+os.sep+'database'+os.sep+'step3'+os.sep+dbfilename+'_HPI_tokenized.csv'
+# write tokenized  'History of Present Illness' text file - step3
+hpi_tokenized_filename = dbpath+os.sep+'step3'+os.sep+dbfilename+'_HPI_tokenized.csv'
 write_file(hpi_list_tokenized,hpi_tokenized_filename)
 
-#write stop-words removed
-hpi_stpwd_rm_filename = '..'+os.sep+'database'+os.sep+'step4'+os.sep+dbfilename+'_HPI_stpwd_rm.csv'
+#write stop-words removed - step4
+hpi_stpwd_rm_filename = dbpath+os.sep+'step4'+os.sep+dbfilename+'_HPI_stpwd_rm.csv'
 write_file(hpi_list_stpwd_rm,hpi_stpwd_rm_filename)
 
 pass
