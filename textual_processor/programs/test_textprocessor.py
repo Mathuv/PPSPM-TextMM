1c73f5a537a87a3d991451034af2f3c5b4042f69
Mathu Mounasamy <mmounasamy@gmail.com>
2016-06-15 21:21:21 +1000

    Modified to make the code comforms to PEP standard, and made it look more generic code than MIMIC III dataset specific

--- textual_processor/programs/test_textprocessor.py
+++ textual_processor/programs/test_textprocessor.py
@@ -1,3 +1,3 @@
 import textprocessor
 
-textprocessor.main('../database/NOTEEVENTS_DATA_TABLE_PARTIAL_20REC.csv','ROW_ID','TEXT','History of Present Illness',10)
+textprocessor.main('../database/NOTEEVENTS_DATA_TABLE_PARTIAL_20REC.csv', 1, 11, 'History of Present Illness:', 10)
--- textual_processor/programs/textprocessor.py
+++ textual_processor/programs/textprocessor.py
@@ -12,25 +12,30 @@ from nltk.corpus import stopwords
 
 
 # extract History of Present Illness - step 2
-def extract_hpi(text):
-    match = re.search(r'History of Present Illness:\s+((\S+\s)+)',text,re.IGNORECASE)
-    return match.group(1) if match  else match
+def extract_text(text,text_section_identifier):
+    # match = re.search(r'History of Present Illness:\s+((\S+\s)+)',text,re.IGNORECASE)
+    match = re.search(r''+text_section_identifier+'\s+((\S+\s)+)', text, re.IGNORECASE)
+    return match.group(1) if match else match
+
 
 # tokenize text into list of words - step 3
 def tokenize(text):
-    #return re.split(r'[ \t\n]+', text) if text else text
+    # return re.split(r'[ \t\n]+', text) if text else text
     return nltk.word_tokenize(text) if text else text
+
 
 # remove stop words - step 4
 def remove_stopwords(tokens):
     stopwords = nltk.corpus.stopwords.words('english')
     return [word for word in tokens if word not in stopwords] if tokens else tokens
 
+
 # stemming - step 5 
 def stem(tokens):
     porter = nltk.PorterStemmer()
     lancaster = nltk.LancasterStemmer()
     return [porter.stem(word) if str(word).isalpha() else word for word in tokens ] if tokens else tokens # please correct this.
+
 
 # tagging
 def pos_tagging(tokens):
@@ -41,13 +46,16 @@ def pos_tagging(tokens):
 def tf(word, tokens):
     return tokens.count(word) / len(tokens)
 
+
 # Num of records containing a word - Step 6.2
 def n_containing(word, textlist):
     return sum(1 for blob in textlist if word in textlist)
 
+
 # Calculate IDF - Step 6.3
 def idf(word, textlist):
     return math.log(len(textlist) / (1 + n_containing(word, textlist)))
+
 
 # Calculate TF-IDF - Step 6.4
 def tfidf(word, tokens, textlist):
@@ -78,127 +86,108 @@ def write_file(content, file):
     f.close()
 
 
-def main(dbfile, id_column_header, text_column_header, text_section_header, m):
-    #dbfile = sys.argv[1]
-    #dbfile = '../database/NOTEEVENTS_DATA_TABLE_PARTIAL_20REC.csv'
+def main(dbfile, id_column_no, text_column_no, text_section_identifier, m):
+
     dbpath = os.path.dirname(dbfile)
     dbfilename_ext = os.path.basename(dbfile)
     dbfilename = os.path.splitext(dbfilename_ext)[0]
     dbreader = csv.reader(open(dbfile))
     dbdata = list(dbreader)
 
+    header_rec = dbdata[0] # Patient table column headers
 
-    header_rec = dbdata[0]  #Patient table column headers
-
-    try:
-        id_column_no = header_rec.index(id_column_header)
-    except:
-        print id_column_header + ' is not found in ' + dbfilename_ext
-        raise
-
-    try:
-        text_column_no = header_rec.index(text_column_header)
-    except ValueError:
-        print text_column_header + ' is not found in ' + dbfilename_ext
-        raise
+    assert len(header_rec) >= int(id_column_no) > 0, 'id column number is out of range'
+    assert len(header_rec) >= int(text_column_no) > 0, 'text column number is out of range'
 
     dsr_list = []
-    hpi_orginal = []
-    hpi_list_tokenized = []
-    hpi_list_stpwd_rm = []
-    hpi_list_stemmed = []
-    hpi_list_pos_tagged = []
-    hpi_list_tfidf = []
-    hpi_list_stpwd_rm_tfidf = []
-    hpi_list_m_tokens = []
-
+    text_orginal = []
+    text_list_tokenized = []
+    text_list_stpwd_rm = []
+    text_list_stemmed = []
+    text_list_pos_tagged = []
+    text_list_tfidf = []
+    text_list_stpwd_rm_tfidf = []
+    text_list_m_tokens = []
 
     for row in dbdata[1:]:
-        #unprocessed data
-        dsr_list.append(row[id_column_no::text_column_no])
+        # unprocessed data
+        dsr_list.append(row[id_column_no-1::text_column_no-1])
 
         # extract Histry of Present Illness - step 2
-        row[text_column_no] = extract_hpi(row[text_column_no])
-        hpi_orginal.append(row[id_column_no::text_column_no])
+        row[text_column_no-1] = extract_text(row[text_column_no-1],text_section_identifier)
+        text_orginal.append(row[id_column_no-1::text_column_no-1])
 
         # cleaning data - convert to lower case
-        row[text_column_no] = row[text_column_no].lower()
+        row[text_column_no-1] = row[text_column_no-1].lower()
 
         # cleaning data - punctuation removal
-        row[text_column_no] = str(row[text_column_no]).translate(None, string.punctuation)
+        row[text_column_no-1] = str(row[text_column_no-1]).translate(None, string.punctuation)
 
         # create tokenized list - step 3
-        row[text_column_no] = tokenize(row[text_column_no])
-        hpi_list_tokenized.append(row[id_column_no::text_column_no])
+        row[text_column_no-1] = tokenize(row[text_column_no-1])
+        text_list_tokenized.append(row[id_column_no-1::text_column_no-1])
 
         # create stop word removed list - step 4
-        row[text_column_no] = remove_stopwords(row[text_column_no])
-        hpi_list_stpwd_rm.append(row[id_column_no::text_column_no])
+        row[text_column_no-1] = remove_stopwords(row[text_column_no-1])
+        text_list_stpwd_rm.append(row[id_column_no-1::text_column_no-1])
 
-        #pos tagging
+        # pos tagging
         # row[10] = pos_tagging(row[10])
-        # hpi_list_pos_tagged.append(row[0::10])
+        # text_list_pos_tagged.append(row[0::10])
 
-        #create stemmed list
-        row[text_column_no] = stem(row[text_column_no])
-        hpi_list_stemmed.append(row[id_column_no::text_column_no])
+        # create stemmed list
+        row[text_column_no-1] = stem(row[text_column_no-1])
+        text_list_stemmed.append(row[id_column_no-1::text_column_no-1])
 
     # TF-IDF calculation - Step 6
-    for rec in hpi_list_stemmed:
-        scores = {token: tfidf(token,rec[1],[l[1] for l in hpi_list_stemmed]) for token in rec[1]}
+    for rec in text_list_stemmed:
+        scores = {token: tfidf(token,rec[1],[l[1] for l in text_list_stemmed]) for token in rec[1]}
         sorted_words = sorted(scores.items(), key=lambda x: x[1], reverse=True)
-        hpi_list_tfidf.append([rec[0],sorted_words])
+        text_list_tfidf.append([rec[0],sorted_words])
         # top m tokens with highest tf_idf score
-        hpi_list_m_tokens.append([rec[0],sorted_words[:int(m)]])
+        text_list_m_tokens.append([rec[0],sorted_words[:int(m)]])
         
     # TF-IDF calculation before stemming - Step 6b
-    for rec in hpi_list_stpwd_rm:
-        scores = {token: tfidf(token, rec[1], [l[1] for l in hpi_list_stpwd_rm]) for token in rec[1]}
+    for rec in text_list_stpwd_rm:
+        scores = {token: tfidf(token, rec[1], [l[1] for l in text_list_stpwd_rm]) for token in rec[1]}
         sorted_words = sorted(scores.items(), key=lambda x: x[1], reverse=True)
-        hpi_list_stpwd_rm_tfidf.append([rec[0], sorted_words])
-
-
+        text_list_stpwd_rm_tfidf.append([rec[0], sorted_words])
 
     # write discharge summery text csv - step 1
     raw_filename = dbpath+os.sep+'step1'+os.sep+dbfilename+'_RAW.csv'
     write_file(dsr_list,raw_filename)
 
-
     # write 'History of Present Illness' text csv - step 2
-    hpi_filename = dbpath+os.sep+'step2'+os.sep+dbfilename+'_HPI.csv'
-    write_file(hpi_orginal,hpi_filename)
-
+    text_filename = dbpath+os.sep+'step2'+os.sep+dbfilename+'_HPI.csv'
+    write_file(text_orginal,text_filename)
 
     # write tokenized  'History of Present Illness' text file - step 3
-    hpi_tokenized_filename = dbpath+os.sep+'step3'+os.sep+dbfilename+'_HPI_tokenized.csv'
-    write_file(hpi_list_tokenized,hpi_tokenized_filename)
+    text_tokenized_filename = dbpath+os.sep+'step3'+os.sep+dbfilename+'_HPI_tokenized.csv'
+    write_file(text_list_tokenized,text_tokenized_filename)
 
-    #write stop-words removed - step 4
-    hpi_stpwd_rm_filename = dbpath+os.sep+'step4'+os.sep+dbfilename+'_HPI_stpwd_rm.csv'
-    write_file(hpi_list_stpwd_rm,hpi_stpwd_rm_filename)
+    # write stop-words removed - step 4
+    text_stpwd_rm_filename = dbpath+os.sep+'step4'+os.sep+dbfilename+'_HPI_stpwd_rm.csv'
+    write_file(text_list_stpwd_rm,text_stpwd_rm_filename)
 
     # write stemmed list - step 5
-    hpi_stemmed_filename = dbpath + os.sep + 'step5' + os.sep + dbfilename + '_HPI_stemmed.csv'
-    write_file(hpi_list_stemmed, hpi_stemmed_filename)
+    text_stemmed_filename = dbpath + os.sep + 'step5' + os.sep + dbfilename + '_HPI_stemmed.csv'
+    write_file(text_list_stemmed, text_stemmed_filename)
 
     # write pos tagged - step
-    # hpi_pos_tagged_filename = dbpath + os.sep + 'step6' + os.sep + dbfilename + '_HPI_tagged.csv'
-    # write_file(hpi_list_pos_tagged, hpi_pos_tagged_filename)
+    # text_pos_tagged_filename = dbpath + os.sep + 'step6' + os.sep + dbfilename + '_HPI_tagged.csv'
+    # write_file(text_list_pos_tagged, text_pos_tagged_filename)
 
     # write if-idf output - Step 6
-    hpi_tfidf_filename = dbpath + os.sep + 'step6' + os.sep + dbfilename + '_HPI_tfidf.csv'
-    write_file(hpi_list_tfidf, hpi_tfidf_filename)
+    text_tfidf_filename = dbpath + os.sep + 'step6' + os.sep + dbfilename + '_HPI_tfidf.csv'
+    write_file(text_list_tfidf, text_tfidf_filename)
 
     # write if-idf output - Step 6b
-    hpi_stpwd_rm_tfidf_filename = dbpath + os.sep + 'step6b' + os.sep + dbfilename + '_HPI_stpwd_rm_tfidf.csv'
-    write_file(hpi_list_stpwd_rm_tfidf, hpi_stpwd_rm_tfidf_filename)
+    text_stpwd_rm_tfidf_filename = dbpath + os.sep + 'step6b' + os.sep + dbfilename + '_HPI_stpwd_rm_tfidf.csv'
+    write_file(text_list_stpwd_rm_tfidf, text_stpwd_rm_tfidf_filename)
     
-    # DV: as a next step (Step 7) select the top m words for each record and write in a csv file, so that we can compare the selected words before hash-mapping them
     # write top m tokens with high if-idf score - Step 7
-    hpi_m_tfidf_filename = dbpath + os.sep + 'step7' + os.sep + dbfilename + '_HPI_m_tfidf.csv'
-    write_file(hpi_list_m_tokens, hpi_m_tfidf_filename)
-
-
+    text_m_tfidf_filename = dbpath + os.sep + 'step7' + os.sep + dbfilename + '_HPI_m_tfidf.csv'
+    write_file(text_list_m_tokens, text_m_tfidf_filename)
 
 if __name__ == "__main__":
     main(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5])
