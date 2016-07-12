eead1dabbf57c2daf9124432d93dd2cb06837c07
Mathu Mounasamy <mmounasamy@gmail.com>
2016-07-12 16:14:08 +1000

    Added support for creating two coutning bloom filters with both tf and idf values

--- textual_processor/programs/TBF.py
+++ textual_processor/programs/TBF.py
@@ -10,15 +10,16 @@ class TBF(object):
         self.l = l # length of bloom filter
         self.k = k # number of hash functions
 
-        self.cbf = [0] * l
+        self.cbf_tf = [0] * l
+        self.cbf_idf = [0] * l
 
         self.h1 = hashlib.sha1
         self.h2 = hashlib.md5
 
     # add list of tokens (textual data) and corresponding token frequencies
-    def add_list(self, val_list, freq_list):
+    def add_list(self, term_list, tf_list, idf_list):
 
-        for val in val_list:
+        for val in term_list:
             hex_str1 = self.h1(val).hexdigest()
             int1 = int(hex_str1, 16)
             hex_str2 = self.h2(val).hexdigest()
@@ -27,9 +28,12 @@ class TBF(object):
             for i in range(self.k):
                 gi = int1 + i * int2
                 gi = int(gi % self.l)
-                self.cbf[gi] += freq_list[val_list.index(val)]
+                # create counting bloom filter with tf values
+                self.cbf_tf[gi] += tf_list[term_list.index(val)]
+                # create counting bloom filter with idf values
+                self.cbf_idf[gi] += idf_list[term_list.index(val)]
 
-        return self.cbf
+        return self.cbf_tf, self.cbf_idf
 
     # Add a single token to the counting bloom filter
     def add_item(self, item):
@@ -41,7 +45,7 @@ class TBF(object):
         for i in range(self.k):
             gi = int1 + i * int2
             gi = int(gi % self.l)
-            self.cbf[gi] += 1
+            self.cbf_tf[gi] += 1
 
     # query an item's existence and frequency
     def query_item(self, item):
@@ -57,14 +61,14 @@ class TBF(object):
             gi = int1 + i * int2
             gi = int(gi % self.l)
 
-            if self.cbf[gi] >= 1:
-                freqs.append(self.cbf[gi])
+            if self.cbf_tf[gi] >= 1:
+                freqs.append(self.cbf_tf[gi])
             else:
                 return False, 0
 
         return True, min(freqs)
 
-    # remove an item from cbf
+    # remove an item from cbf_tf
     def remove_item(self, item):
 
         hex_str1 = self.h1(item).hexdigest()
@@ -75,7 +79,7 @@ class TBF(object):
         for i in range(self.k):
             gi = int1 + i * int2
             gi = int(gi % self.l)
-            self.cbf[gi] -= 1
+            self.cbf_tf[gi] -= 1
 
     # Calculate (Dice's coefficient) similarity  between a counting bloom filter and a list of tokens.
     def cal_dc_sim_list(self, len_list1, list2):
@@ -104,14 +108,14 @@ class TBF(object):
         return sim
 
     # calculate dissimilarity between two counting bloom filters (between two textual data)
-    def cal_dissim_cbf(self, cbf2):
+    def cal_dissim_cbf_tf(self, cbf2):
 
-        assert len(self.cbf) != len(cbf2), 'bloom filters are not of same length'
+        assert len(self.cbf_tf) != len(cbf2), 'bloom filters are not of same length'
 
-        if len(self.cbf) == len(cbf2):
+        if len(self.cbf_tf) == len(cbf2):
             sum_diff = 0
             for i in range(self.k):
-                sum_diff += abs(self.cbf[i] - cbf2[i])
+                sum_diff += abs(self.cbf_tf[i] - cbf2[i])
 
             # calculation of dissimilarity
             dissim = sum_diff / (2 * self.k)
@@ -123,16 +127,19 @@ class TBF(object):
     # calculate dice coefficient similarity between  two counting bloom filters
     def cal_dc_sim_cbf(self, cbf2):
 
-        assert len(self.cbf) != len(cbf2), 'bloom filters are not of same length'
+        assert len(self.cbf_tf) != len(cbf2), 'bloom filters are not of same length'
 
-        if len(self.cbf) == len(cbf2):
+        if len(self.cbf_tf) == len(cbf2):
             sum_min = 0
-            for x,y in zip(self.cbf, cbf2):
+            for x,y in zip(self.cbf_tf, cbf2):
                 sum_min += min(x,y)
         else:
             return None
 
-        return 2 * sum_min / sum(self.cbf) + sum(cbf2)
+        return 2 * sum_min / sum(self.cbf_tf) + sum(cbf2)
+
+
+
 
 
 
