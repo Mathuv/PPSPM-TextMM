2a4d822cca0751a0dbbc45367121224fd558bd9e
Mathu Mounasamy <mmounasamy@gmail.com>
2016-07-28 07:20:44 +1000

    Modified to create db datasets with exact number of records using csv reader, writer

--- textual_processor/util/create_db.py
+++ textual_processor/util/create_db.py
@@ -1,15 +1,32 @@
 import os
+import csv
 
-no_of_records = 1000
+no_of_records = 10000
 no_of_lines = 260000
 source_file_path = '..' + os.sep + '..' + os.sep + '..' + os.sep + 'NOTEEVENTS_DATA_TABLE.csv'
 destination_file_path = '..' + os.sep + 'database' + os.sep + 'NOTEEVENTS_DATA_TABLE_PARTIAL_' + str(no_of_records) + 'REC.csv'
 count = 0
-with open(source_file_path,'r') as src_file:
-    for line in src_file:
-        count += 1
-        with open(destination_file_path,'a') as des_file:
-            des_file.writelines(line)
-        if count == no_of_lines:
-            break
+
+# to include header record
+no_of_records += 1
+
+# with open(source_file_path,'r') as src_file:
+#     for line in src_file:
+#         count += 1
+#         with open(destination_file_path,'a') as des_file:
+#             des_file.writelines(line)
+#         if count == no_of_lines:
+#             break
+
+with open(source_file_path, 'rb') as src_file:
+    filereader = csv.reader(src_file)
+    with open(destination_file_path, 'wb') as des_file:
+        filewriter = csv.writer(des_file)
+        for line in filereader:
+            count += 1
+            filewriter.writerow(line)
+            if count == no_of_records:
+                break
+
+
 
